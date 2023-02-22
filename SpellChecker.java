7e431877c211a2e3104bba1d98170f69b95289df
Mohamed Elzeini <mohamed.elzeini@icloud.com>
2023-02-22 06:51:45 +0200

    Update SpellChecker.java
    added a comment to explain the program function

--- SpellChecker.java
+++ SpellChecker.java
@@ -4,6 +4,13 @@ import java.util.*;
 import java.util.Scanner;
 import javax.swing.JFileChooser;
 
+/*
+ * This program uses two of the classes in the Java Collection Framework: HashSet and TreeSet to implement a spell checker.
+ * English words are listed in the file words.txt, one word per line.
+ * To verify that the words on this list are spelled correctly, the software will look them up.
+ * The words are also kept in a set.
+ */
+
 public class SpellChecker {
 	
 	public static void main(String[] args) throws FileNotFoundException {
